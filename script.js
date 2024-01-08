b099ba02c3af8d4c361974fb43c91b4672582236
Guerschom-Tambwe <guerschomguerschom@gmail.com>
2024-01-08 14:43:44 +0200

    Create a function to fetch Unsplash images

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("testing;")
+const count = 10;
+const apiKey = 'PPAKotSphgtqvOs-sJMx0rRScoRILLx1CXLLEPxjdi4';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+getPhotos();
