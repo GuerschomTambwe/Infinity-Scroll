6b0a77d32631f10f9a298837c7b22f18006dde87
Guerschom-Tambwe <guerschomguerschom@gmail.com>
2024-01-09 11:17:52 +0200

    Refactored the code to meet the DRY principle

--- script.js
+++ script.js
@@ -8,15 +8,25 @@ const count = 10;
 const apiKey = 'PPAKotSphgtqvOs-sJMx0rRScoRILLx1CXLLEPxjdi4';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes){
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 function displayPhotos() {
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank')
+        setAttributes(item, {
+            href: photo.links.html, target: '_blank',
+        })
         
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular, 
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
 
         item.appendChild(img);
         imageContainer.appendChild(item);
